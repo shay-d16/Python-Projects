561ab864ba71dd654965abf43ff3c72a1e21fb4e
Shay R <shalondra.davis@protonmail.com>
2021-04-22 18:03:32 -0700

    Python Assignments

---
+++ if_else.py
@@ -0,0 +1,12 @@
+num1 = 12
+key = False
+
+if num1 == 12:
+    if key:
+        print('Num1 is equal to Twelve and they have the key!')
+    else:
+        print('Num1 is equal to Twelve an they do NOT have the key.')
+elif num1 < 12:
+    print('Num1 is less than Twelve')
+else:
+    print('Num1 is Not equal to Twelve')
