ff97bae699b49497a1cc9477dc79940a3c49ba02
Shay R <shalondra.davis@protonmail.com>
2021-04-25 18:44:21 -0700

    Python Methods
    Utilize the slice(), strip(), and upper() methods, as well as the in keyword in Python.

--- string_variables.py
+++ string_variables.py
@@ -13,8 +13,39 @@ quote = """ \n\"It is worse to stay
     than to wander about lost for a while
     and looking for the psychic
     and soulful kinship one requires\"
-    ― Clarissa Pinkola Estés """
+    ― Clarissa Pinkola Estés\n """
 print(quote)
 
 
-myList = "
+
+
+goddesses = ("Aphrodite", "Athena", "Artemis", "Hera", "Persephone", "Demeter", "Hestia")
+    
+s = slice(3)
+
+print(goddesses[s])
+
+if "Hestia" in goddesses:
+    print("\nSome may not be familiar with the Greek Goddesses Hestia, but she is very important among the other Goddesses.\n")
+
+
+
+
+favGoddess = "    Aphrodite    "
+
+x = favGoddess.strip()
+
+print("\nMy favorite Greek Goddess is", x)
+
+
+love = ("\nI love her soooo much!!").upper()
+print(love)
+
+
+
+goddessStr = "\nMy second favorite Greek Goddess is "
+
+secondFav = "Persephone"
+
+print(goddessStr + secondFav)
+
