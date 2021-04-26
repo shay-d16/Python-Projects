0ece3270fd6008d9e224440361109c87c05f2ad4
Shay R <shalondra.davis@protonmail.com>
2021-04-26 14:28:47 -0700

    Python Lists
    Complete the List challenge.

---
+++ lists.py
@@ -0,0 +1,37 @@
+# Python 3.9.4
+#
+# Author: Shalondra Rossiter
+#
+# This is a set of challenges regarding lists in Python
+# given to me by The Tech Academy.
+
+
+### 1. Create a list.
+myList = ["Gon", "Killua", "Kurapika", "Leorio", "Hisoka"]
+
+
+### 2. Loop through the list with a for loop.
+for i in myList:
+    print(i)
+
+
+### 3. Use the append() method.
+myList.append("Knuckle")
+print(myList)
+
+
+
+### 4. Make a copy of the list. Example: List method copy().
+x = myList.copy()
+print(x)
+
+
+
+### 5. Concatenate two lists
+secondList = ["Morel","Chrollo","Neferpitou","Kite","Biscuit"]
+print(myList + secondList)
+
+
+### 6. Use the reverse() method on a list
+myList.reverse
+print(myList)
